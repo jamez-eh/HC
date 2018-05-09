af6b6080a6698c6766f0fb1c0259ef882e5e4637
Bimal Bhagrath <bimo.17@outlook.com>
2018-05-09 13:41:11 -0400

    Add files via upload

--- PostgREST dpd/display.js
+++ PostgREST dpd/display.js
@@ -1,44 +1,45 @@
 //js file for querying and displaying users selected drug
 
 var drug_id = window.location.search.substring(9);
-var base = "https://rest-dev.hres.ca/rest-dev/dpd_json?select=*";
+var base = "https://rest-dev.hres.ca/rest-dev/dpd_json?select=*&drug_code=eq.";
+var monograph_base = "https://rest-dev.hres.ca/rest-dev/product_monographs?select=*&drug_code=eq.";
 
 $(document).ready(function() {
-	
-	query = base + "&drug_code=eq." + drug_id;
-	
+
+	query = base + drug_id;
+
 	$.get(query, function(data) {
-	
-		console.log(data[0]);
+
+		//console.log(data[0]);
 		var obj = data[0].drug_product;
 		var market_date = "N/A";
-		
+
 		for(var x = 0; x < obj.status.length; x++) {
 			if (obj.status[x].current_status_flag == "Y") {
 				$("#status").html("<b>" + obj.status[x].status + "</b>");
 				$("#statusDate").html(obj.status[x].history_date);
 			}
-			
+
 			if (obj.status[x].status == "MARKETED") {
 				if (obj.status[x].history_date < market_date) {
 					market_date = obj.status[x].history_date;
 				}
 			}
 		}
-		
+
 		$("#market").html(market_date);
 		$("#product").html(obj.brand_name);
 		$("#din").html(obj.drug_identification_number);
-		
+
 		$("#company").html("<b>" + obj.company.company_name + "</b>");
-		
+
 		if (obj.company.suite_number != "") {
 			$("#company").append("<br>" + obj.company.suite_number);
 		}
-		
+
 		$("#company").append("<br>" + obj.company.street_name + "<br>" + obj.company.city_name + ", " + obj.company.province + "<br>" + obj.company.country + " " + obj.company.postal_code);
 		$("#drugClass").html(obj.class);
-		
+
 		if (obj.vet_species) {
 			for (var i = 0; i < obj.vet_species.length; i++) {
 				if (i == 0) {
@@ -48,42 +49,78 @@ $(document).ready(function() {
 					$("#species").append(", " + obj.vet_species[i]);
 				}
 			}
-			
+
 			$("#speciesDiv").css("display", "block");
 		}
-		
+
 		$("#dosage").html(obj.dosage_form[0]);
 		$("#route").html(obj.route[0]);
 		$("#active").html(obj.number_of_ais);
-		
+
 		if (obj.schedule) {
 			$("#schedule").html(obj.schedule[0]);
 		}
-		
+
 		if (obj.therapeutic_class) {
 			$("#ahfs").html(obj.therapeutic_class[0].tc_ahfs_number + " " + obj.therapeutic_class[0].tc_ahfs);
 			$("#atc").html(obj.therapeutic_class[0].tc_atc_number + " " + obj.therapeutic_class[0].tc_atc);
 		}
-		
+
 		$("#aig").text(obj.ai_group_no);
-		
+
 		var table = document.getElementById("activeIngredientsTable");
-		
+
 		for (var i = 0; i < obj.active_ingredients.length; i++) {
 			var ingredientRow = table.insertRow(i + 1);
-			
+
 			var activeIngredientCell = ingredientRow.insertCell(0);
 			var strengthCell = ingredientRow.insertCell(1);
-			
+
 			activeIngredientCell.innerHTML = obj.active_ingredients[i].ingredient;
 			strengthCell.innerHTML = obj.active_ingredients[i].strength + " " + obj.active_ingredients[i].strength_unit;
 		}
-		
+
+		$("#api-call").attr("href", base + "&drug_code=eq." + drug_id).html(base + "&drug_code=eq." + drug_id);
+
 		table.deleteRow(-1);
+	});
+
+	var monograph_query = monograph_base + drug_id;
+
+	$.get(monograph_query, function (data) {
+
+		if (data.length > 0) {
+			var mlink = "https://pdf.hres.ca/dpd_pm/";
+
+			var pm_number = data[0].pm_english_fname
+			var pm = 0;
+
+			for (var i = 1; i < data.length; i++) {
+				if (data[i].pm_english_fname > pm_number) {
+					pm_number = data[i].pm_english_fname;
+					pm = i;
+				}
+			}
+
+			$("#mono").html("<a href='" + mlink + maskCode(data[pm].pm_english_fname, 8) + ".PDF" + "'>Electronic Monograph (" + data[pm].pm_date + ")</a>");
+		}
+		else {
+			$("#mono").html("No Electronic Monograph Available");
+		}
 	});
 });
 
 function newSearch() {
 
 	window.location = "./index.html";
+}
+
+function maskCode(id, length) {
+
+    var code = "" + id;
+    while (code.length < length) {
+        code = "0" + code;
+    }
+
+    return code;
 }
--- PostgREST dpd/request3.js
+++ PostgREST dpd/request3.js
@@ -16,48 +16,48 @@ function prepare() {
 	var input = document.getElementById("search").value;
 	var search = input.split(" ");
 	var illegal = ["of", "&", "and", "?", "!", "or", "+"];
-	
+
 	for(var x = 0; x < illegal.length; x++) {
-		
+
 		var i = $.inArray(illegal[x], search);
-		
+
 		if(i > -1) {
 			search.splice(i, 1);
 		}
 	}
-	
+
 	requestForResource(search);
 }
 
 function requestForResource(search) {
-	
+
 	var drugTable = document.getElementById("drugTable");
-	
+
 	query = base;
 
 	if(search[0] != "") {
 		for(var x = 0; x < search.length; x++) {
-		
+
 			if(search[x].indexOf("(") == -1 && search[x].indexOf(")") == -1) {
 				query += "&search=@@." + search[x];
 			}
 		}
 	}
-	
+
 	//query += "&order=drug_product";
 	query += "&order=drug_product->>brand_name";
-	
+
 	console.log(query);
-	
+
 	$.ajax({
 		url: query,
 		method: "GET",
 		beforeSend: function(xhr) {
 			var low = (page * items);
 			var high = ((page + 1) * items) - 1;
-			
+
 			var range = low + "-" + high;
-			
+
 			xhr.setRequestHeader('Range-Unit', 'items');
 			xhr.setRequestHeader('Range', range);
 			xhr.setRequestHeader('Prefer', 'count=exact');
@@ -70,40 +70,40 @@ function requestForResource(search) {
 }
 
 function createDrugTable(table, data, content) {
-	
+
 	content = content.split("/");
-	
+
 	//console.log(content);
 	//console.log(data[0]);
-	
+
 	if (data.length == 0) {
 		document.getElementById("queryMessage").innerHTML = "<em>No results found</em>";
 		document.getElementById("fetch").style.display = "none";
 	}
 	else {
 		var rowIndex = (page * items);
-		
+
 		if (page == 0) {
 			rowIndex++;
 		}
-	
+
 		var dataIndex = 0;
 		var remaining = content[1] - (page * items);
 		var maxDataIndex = 0;
-		
+
 		if (remaining < items) {
 			maxDataIndex = remaining;
 		}
 		else {
 			maxDataIndex = items;
 		}
-	
+
 		for (dataIndex; dataIndex < maxDataIndex; dataIndex++) {
-			
+
 			var obj = data[dataIndex].drug_product;
-			
+
 			//console.log(obj);
-			
+
 			var drugRow = table.insertRow(rowIndex + 1);
 			var statusCell = drugRow.insertCell(0);
 			var dinCell = drugRow.insertCell(1);
@@ -111,47 +111,61 @@ function createDrugTable(table, data, content) {
 			var productCell = drugRow.insertCell(3);
 			var classCell = drugRow.insertCell(4);
 			var activeCell = drugRow.insertCell(5);
-			var strenghtCell = drugRow.insertCell(6);
-			
+			//var strenghtCell = drugRow.insertCell(6);
+
 			for(var x = 0; x < obj.status.length; x++) {
-				
+
 				if (obj.status[x].current_status_flag == "Y") {
 					statusCell.innerHTML = obj.status[x].status;
 				}
 			}
-			
+
 			dinCell.innerHTML = "<a href='./drug-id.html?product=" + obj.drug_code + "'>" + obj.drug_identification_number + "</a>";
 			companyCell.innerHTML = obj.company.company_name;
 			productCell.innerHTML = obj.brand_name;
 			classCell.innerHTML = obj.class;
-			
+
+			/*
 			var s = 0;
 			var i = 0;
-			
+
 			for(var x = 0; x < obj.active_ingredients.length; x++) {
-				
+
 				if(obj.active_ingredients[x].strength > s) {
 					s = obj.active_ingredients[x].strength;
 					i = x;
 				}
 			}
-			
+
 			activeCell.innerHTML = obj.active_ingredients[i].ingredient;
 			strenghtCell.innerHTML = obj.active_ingredients[i].strength + " " + obj.active_ingredients[i].strength_unit;
-			
+			*/
+
+			var activeText = "";
+
+			for(var i = 0; i < obj.active_ingredients.length; i++) {
+				activeText += obj.active_ingredients[i].ingredient + " (" + obj.active_ingredients[i].strength + " " + obj.active_ingredients[i].strength_unit + ")";
+
+				if (i != obj.active_ingredients.length - 1) {
+					activeText += ", "
+				}
+			}
+
+			activeCell.innerHTML = activeText;
+
 			rowIndex++;
 		}
 	}
-	
+
 	document.getElementById("fetch").innerHTML = "Load More (" + (remaining - items) + " items remaining)";
 	document.getElementById("queryResults").style.display = "block";
 	document.getElementById("queryFields").style.display = "none";
-	
+
 	if (page == 0) {
 		table.deleteRow(1);
 		window.scrollTo(0,0);
 	}
-	
+
 	if (remaining <= items) {
 		document.getElementById("fetch").style.display = "none";
 	}
@@ -160,18 +174,18 @@ function createDrugTable(table, data, content) {
 function fetch() {
 
 	page++;
-	
+
 	var drugTable = document.getElementById("drugTable");
-	
+
 	$.ajax({
 		url: query,
 		method: "GET",
 		beforeSend: function(xhr) {
 			var low = (page * items);
 			var high = ((page + 1) * items) - 1;
-			
+
 			var range = low + "-" + high;
-			
+
 			xhr.setRequestHeader('Range-Unit', 'items');
 			xhr.setRequestHeader('Range', range);
 			xhr.setRequestHeader('Prefer', 'count=exact');
